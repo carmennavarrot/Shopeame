113cef260baf8f154311880feac60194445236e2
carmennavarrot <carmen.navarro.tornay@bootcamp-upgrade.com>
2024-06-11 21:17:12 +0200

    mejoras en el formulario

--- src/app/interfaces/product.ts
+++ src/app/interfaces/product.ts
@@ -1,5 +1,4 @@
 export interface Product {
-length: any
 id: number
 name: string
 price: number
--- src/app/pages/admid/admid.component.ts
+++ src/app/pages/admid/admid.component.ts
@@ -13,42 +13,88 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './admid.component.css'
 })
 export class AdmidComponent {
- product: Product | null = null;
+
+product: Product | null = null;
 formulario!: FormGroup;
 parametro: string | null = "";
 
-constructor(private formBuilder: FormBuilder,
+
+constructor(private builder: FormBuilder,
   private productService: ProductService,
   private route: ActivatedRoute
   // route activatedroute me permite utilizar 
 ){
-  this.formulario = formBuilder.group({
-"producto": new FormControl(null, [Validators.required ,]),
- "Precio": new FormControl(null, [Validators.required, /*Validators.pattern*/]),
- "Descripcion": new FormControl(null, []),
- "Imagen":new FormControl(null,[Validators.required]),
- "Opiniones":new FormControl(null, [/*Validators.pattern*/])
-
-  })
    route.paramMap.subscribe(params=>{
     this.parametro =params.get('id');
    });
 
    if(this.parametro === "nuevo"){
+    this.product = {
+      description: "",
+      price: 0,
+      id: 0,
+      image: "",
+      name: "",
+      stars: 0,
+    }
+    this.construirFormulario(builder)
 
    }else{
     productService.getById(Number(this.parametro)).subscribe({
       next: (response)=>{
         this.product = response as Product
+        this.construirFormulario(builder)
       },
       error: ()=>{}
     })
    }
 
 }
-
+construirFormulario(builder: FormBuilder){
+  if(this.product){
+    this.formulario = builder.group({
+      "producto": new FormControl(this.product.name, []) , 
+      "Precio": new FormControl(this.product.price, []), 
+      "Descrip":new FormControl(this.product.description, []) , 
+      "Imagen": new FormControl(this.product.image, []), 
+       "Opiniones": new FormControl(this.product.stars, [Validators.max(5), Validators.min(0)]), 
+    })
+  }
+  
+}
 enviarFormulario() {
   console.log('enviando formulario...')
+}
+
+onCambioValoracion(){
+ const stars = Number( this.formulario.value.review)
+ if(this.product){
+  this.product.stars = stars
+ }
+}
+onCambioPrecio(){
+  const price = Number( this.formulario.value.price)
+  if(this.product){
+   this.product.price = price
+  }
+}
+onCambioImagen(){
+  const img =  this.formulario.value.image
+  if(this.product){
+   this.product.image = img
+  }
+}
+onCambioDescrip(){
+  const descip = this.formulario.value.description
+  if(this.product){
+    this.product.description = descip
+  }
+}
+onCambioProduct(){
+  const prod = this.formulario.value.name
+  if(this.product){
+    this.product.name = prod
+  }
 }
 
 }
